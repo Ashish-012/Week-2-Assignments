6b5d1e623782fd24fa548039cc4a8b2e26a80fc1
Ashish-012 <contactashishjoshi99@gmail.com>
2023-06-20 17:39:53 +0530

    passed all test for fileServer

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -16,10 +16,35 @@
 
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
+// const port = 3000;
 
+app.get("/files", (req, res) => {
+  fs.readdir("./files", (err, files) => {
+    if (err) {
+      res.status(500).send();
+    } else {
+      res.status(200).json(files);
+    }
+  });
+});
+
+app.get("/file/:filename", (req, res) => {
+  const fileName = req.params.filename;
+  fs.readFile(`./files/${fileName}`, (err, data) => {
+    if (err) {
+      res.status(404).send("File not found");
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Route not found");
+});
 
 module.exports = app;
